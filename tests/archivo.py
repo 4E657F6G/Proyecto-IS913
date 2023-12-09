df52842cb2223d5fdeea839c557bd77aef864df0
DeathKira <jeff23avila@gmail.com>
2023-12-08 23:33:14 -0600

    Update archivo.py

--- tests/archivo.py
+++ tests/archivo.py
@@ -5,10 +5,15 @@ b = 1.3
 t = "fg"
 
 if a > b:
-    a = 5
+    print("a > b")
+else:
+    print("HOla")
 
+if a > b:
+    print("a > b")
 
 while(a>b):
     n = 7
-    a = 1
+    a = a + 1
+        
         
